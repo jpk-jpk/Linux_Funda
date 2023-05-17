db297e42d9f6e23968922e9139b131d20bafa651
Pradeep Kumar Jangam <jpradeepkumar.jpk@gmail.com>
2023-05-17 05:00:50 +0000

    Code correction

--- countdown.sh
+++ countdown.sh
@@ -2,10 +2,9 @@
 
 COUNTER=$1
 COUNTER=$((COUNTER*60))
-while true
+while((COUNTER > -1))
 do 
     echo $COUNTER seconds to the countdown
     COUNTER=$((COUNTER-1))
     sleep 1
 done
-echo TIME TO START
