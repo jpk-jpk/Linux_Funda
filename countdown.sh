860eeb74a55cecfa4542ab4e814f9b2ca72db315
Pradeep Kumar Jangam <jpradeepkumar.jpk@gmail.com>
2023-05-17 04:42:08 +0000

    COde changes

--- countdown.sh
+++ countdown.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 COUNTER=$1
-COUNTER=$((COUNTER * 60))
+COUNTER=$((COUNTER*60))
 while true
 do 
     echo $COUNTER seconds to the countdown
-    COUNTER = $((COUNTER - 1))
+    COUNTER=$((COUNTER-1))
     sleep 1
 done
+echo TIME TO START
